23c8867705cce966cd48ff05e022394a2006f910
KRISHAL DANKHARA <dankharakrishal2005@gmail.com>
2025-02-21 23:03:56 +0530

    create menu icon for navbar for better responsiveness and made website responsive for home and feature page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,10 +3,13 @@ import './Navbar.css'
 import logo from '../../assets/cryptocurrency.png'
 import { CoinContext } from '../../Context/CoinContext'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import menu from '../../assets/menu.png'
 
 const Navbar = () => {
 
   const {setCurrency} = useContext(CoinContext)
+  const [showMenu, setShowMenu] = useState(false);
 
   const currencyHandler = (event) => {
     switch (event.target.value) {
@@ -55,6 +58,12 @@ const Navbar = () => {
                 <option value="inr">INR</option>
             </select>
         </div>
+        <img src={menu} alt="Menu" className='mobMenu' onClick={() =>setShowMenu(!showMenu)}/>
+                <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+                    <Link activeClass='active' to={'/'} spy={true} smooth={true} offset={-100} duration={500} className='listItem' onClick={() =>setShowMenu(false)}>Home</Link>
+                    <Link activeClass='active' to={'/features'} spy={true} smooth={true} offset={-70} duration={500} className='listItem' onClick={() =>setShowMenu(false)}>Features</Link>
+                    <Link activeClass='active' to={'/blog'} spy={true} smooth={true} offset={-100} duration={500} className='listItem' onClick={() =>setShowMenu(false)}>Blog</Link>
+                </div> 
     </div>
   )
 }
